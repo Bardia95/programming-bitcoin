72d14894ada2c6ae842b515d9fd4322bebd0aa72
Bardia Pourvakil <bardia.95.pourvakil@gmail.com>
2020-05-01 19:11:58 -0400

    Init commit

---
+++ test/programming_bitcoin/core_test.clj
@@ -0,0 +1,7 @@
+(ns programming-bitcoin.core-test
+  (:require [clojure.test :refer :all]
+            [programming-bitcoin.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
