574bb4864ad60c51711a38d8df8d684c08c0586e
Bardia Pourvakil <bardia.95.pourvakil@gmail.com>
2020-06-03 12:19:17 -0400

    scalar multiplication + tests + refactor

--- test/programming_bitcoin/ecc_test.clj
+++ test/programming_bitcoin/ecc_test.clj
@@ -6,6 +6,12 @@
 
 
 (deftest exponentiation
+  (testing "pwr"
+    (testing "integers"
+      (is (= (pwr 3 2) 9))
+      (is (= (pwr 0 1) 0))
+      (is (= (pwr 2 0) 1))
+      (is (= (pwr 2 1) 2))))
   (testing "mod-expt"
     (testing "small positive integers"
       (is (= (mod-expt 2 5 7)  (.modPow (biginteger 2) (biginteger 5) (biginteger 7)))))
@@ -26,10 +32,6 @@
       (is (= (prime? 290245329165570025116016487217740287508837913295571609463914348778319654489118435855243301969001872061575755804802874062021927719647357060447135321577028929269578574760547268310055056867386875959045119093967972205124270441648450825188877095173754196346551952542599226295413057787340278528252358809329N) true)))))
 
 (deftest field-elements
-  (testing "make-fe"
-    (is (= (make-fe 0 7)  (->FieldElement 0 7)))
-    (is (= (make-fe 1 7)  (->FieldElement 1 7)))
-    (is (= (make-fe 6 31) (->FieldElement 6 31))))
   (testing "field-ops"
     (let [p 31
           a (->FieldElement 2 p)
@@ -56,23 +58,23 @@
         (is (= (- c f) (->FieldElement 16 31))))
       (testing "*"
         (is (= (* g h) (->FieldElement 22 31))))
-      (testing "expt"
-        (is (= (expt d 3)         (->FieldElement 15 31)))
-        (is (= (* m (expt l 5)))) (->FieldElement 16 31))
+      (testing "pwr"
+        (is (= (pwr d 3)         (->FieldElement 15 31)))
+        (is (= (* m (pwr l 5)))) (->FieldElement 16 31))
       (testing "/"
         (is (= (/ i g)            (->FieldElement 4 31)))
-        (is (= (expt d -3)        (->FieldElement 29 31)))))))
+        (is (= (pwr d -3)        (->FieldElement 29 31)))))))
 
 (deftest curve-points
-  (testing "make-pt"
-    (is (= (make-pt -1 -1 5 7)       (->Point -1 -1 5 7)))
-    (is (= (make-pt 18 77 5 7)       (->Point 18 77 5 7)))
-    (is (= (make-pt ##Inf ##Inf 5 7) (->Point ##Inf ##Inf 5 7))))
-  (testing "point-ops"
+  (testing "->Point"
+    (is (= (->Point -1 -1 5 7)       (map->Point {:x -1 :y -1 :a 5 :b 7})))
+    (is (= (->Point 18 77 5 7)       (map->Point {:x 18 :y 77 :a 5 :b 7})))
+    (is (= (->Point ##Inf ##Inf 5 7) (map->Point {:x ##Inf :y ##Inf :a 5 :b 7}))))
+  (testing "point addition over reals"
     (let [a (->Point ##Inf ##Inf 5 7)
-          b (->Point 2 5 5 7)
-          c (->Point 2 -5 5 7)
-          d (->Point 3 7 5 7)
+          b (->Point  2  5 5 7)
+          c (->Point  2 -5 5 7)
+          d (->Point  3  7 5 7)
           e (->Point -1 -1 5 7)
           f (->Point 18 77 5 7)]
       (testing "point at infinity + point"
@@ -83,4 +85,38 @@
       (testing "different points"
         (is (= (+ d e) c)))
       (testing "same point"
-        (is (= (+ e e) f))))))
+        (is (= (+ e e) f)))))
+  (let [prime 223
+        a  (->FieldElement   0 prime)
+        b  (->FieldElement   7 prime)
+        x1 (->FieldElement 192 prime)
+        y1 (->FieldElement 105 prime)
+        x2 (->FieldElement  17 prime)
+        y2 (->FieldElement  56 prime)
+        x3 (->FieldElement 170 prime)
+        y3 (->FieldElement 142 prime)
+        x4 (->FieldElement  47 prime)
+        y4 (->FieldElement  71 prime)
+        x5 (->FieldElement  36 prime)
+        y5 (->FieldElement 111 prime)
+        x6 (->FieldElement  15 prime)
+        y6 (->FieldElement  86 prime)
+        inf (->Point ##Inf ##Inf a b)
+        p1  (->Point    x1   y1  a b)
+        p2  (->Point    x2   y2  a b)
+        p3  (->Point    x3   y3  a b)
+        p4  (->Point    x4   y4  a b)
+        p5  (->Point    x5   y5  a b)
+        p6  (->Point    x6   y6  a b)]
+    (testing "point addition over finite fields"
+      (testing "different points"
+        (is (= (+ p1 p2) p3)))
+      (testing "same points"
+        (is (= (+ p4 p4) p5))))
+    (testing "scalar multiplication"
+      (testing "multiplying by 0"
+        (is (= (* 0 p1) inf)))
+      (testing "multiplying by 1"
+        (is (= (* 1 p4) p4)))
+      (testing "multiplying until point at infinity"
+        (is (= (* 7 p6) inf))))))
